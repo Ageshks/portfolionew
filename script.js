895d04bbb36c1d4df6cdeda617609882a961bcdf
Agesh <macbeth@Ageshs-MacBook-Air.local>
2026-05-11 03:20:40 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-function openLink(url) {
-  if (url !== '#') {
-    window.open(url, '_blank');
-  }
+let zIndex = 10;
+
+function bringToFront(el){
+  el.style.zIndex = ++zIndex;
 }
 
-// Live time
-setInterval(() => {
+function openApp(id){
+  const el = document.getElementById(id);
+  el.style.display = "flex";
+  bringToFront(el);
+}
+
+function closeApp(id){
+  document.getElementById(id).style.display = "none";
+}
+
+function minimizeApp(id){
+  document.getElementById(id).style.display = "none";
+}
+
+function toggleMaximize(id){
+  document.getElementById(id).classList.toggle("maximized");
+}
+
+/* DRAG */
+function drag(e, el){
+  const x = e.clientX - el.offsetLeft;
+  const y = e.clientY - el.offsetTop;
+
+  function move(ev){
+    el.style.left = (ev.pageX - x) + "px";
+    el.style.top  = (ev.pageY - y) + "px";
+  }
+
+  document.addEventListener("mousemove", move);
+  document.onmouseup = ()=> document.removeEventListener("mousemove", move);
+}
+
+/* TIME */
+setInterval(()=>{
   const now = new Date();
   document.getElementById("time").innerText =
-    now.toLocaleDateString() + " " + now.toLocaleTimeString();
-}, 1000);
+    now.toLocaleTimeString();
+},1000);
